8ae9c2917012aba824d6cf6c9c5aa3aec1f969c8
emap-strawberry <melon.pan.san@gmail.com>
2024-11-19 10:55:01 +0900

    style: change button and background colors

--- frontend/src/ResetGame.jsx
+++ frontend/src/ResetGame.jsx
@@ -1,3 +1,11 @@
 export default function ResetGame({ onClick }) {
-  return <button onClick={resetGame}>もう一度遊ぶ</button>;
+  // リロードで初期化
+  const resetGame = () => {
+    window.location.reload();
+  };
+  return (
+    <button onClick={resetGame} className="btn">
+      もう一度遊ぶ
+    </button>
+  );
 }
